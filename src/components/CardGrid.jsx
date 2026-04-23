b2c6a5627f87e59012e45413ec30d9f9d38e01a4
themythicden <themythicden@gmail.com>
2026-04-23 04:21:42 +0200

    Update CardGrid.jsx

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,13 +1,28 @@
 import Card from "./Card";
 
 export default function CardGrid({ cards, userCards, setFilter, onAdd, onRemove }) {
+
+  function buildUserData(cardId, userCards) {
+  const data = {};
+
+  Object.keys(userCards).forEach(key => {
+    const [id, variant] = key.split("_");
+
+    if (id === cardId) {
+      data[variant] = userCards[key]?.total || 0;
+    }
+  });
+
+  return data;
+}
+  
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
       {cards.map(card => (
         <Card
           key={card.id}
           card={card}
-          userData={userCards[card.id] || {}}
+          userData={buildUserData(card.id, userCards}
           setFilter={setFilter}
           onAdd={onAdd}
           onRemove={onRemove}
@@ -16,3 +31,4 @@ export default function CardGrid({ cards, userCards, setFilter, onAdd, onRemove 
     </div>
   );
 }
+
