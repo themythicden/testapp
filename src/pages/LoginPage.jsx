c8225728c8532f8cada1b7f1b6d8587a7f566301
themythicden <themythicden@gmail.com>
2026-04-23 03:36:10 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -44,40 +44,45 @@ export default function LoginPage() {
     <div className="flex justify-center items-center h-[80vh]">
       <div className="bg-white p-6 shadow w-80">
         <h2 className="text-xl mb-4">Login</h2>
-
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full border p-2 mb-2"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full border p-2 mb-4"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-
-        {error && (
-          <div className="text-red-500 mb-2">{error}</div>
-        )}
-
-        <button
-          onClick={handleLogin}
-          className="w-full bg-black text-white p-2 mb-2"
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
         >
-          Login
-        </button>
-
-        <button
-          onClick={handleSignup}
-          className="w-full border p-2"
-        >
-          Sign Up
-        </button>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full border p-2 mb-2"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+    
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full border p-2 mb-4"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+    
+            {error && (
+              <div className="text-red-500 mb-2">{error}</div>
+            )}
+    
+            <button
+              onClick={handleLogin}
+              className="w-full bg-black text-white p-2 mb-2"
+            >
+              Login
+            </button>
+        </form>
+            <button
+              onClick={handleSignup}
+              className="w-full border p-2"
+            >
+              Sign Up
+            </button>
       </div>
     </div>
   );
