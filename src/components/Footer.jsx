4cb1c51fc60fb8128de96e3625c3848991be8bc6
themythicden <themythicden@gmail.com>
2026-04-25 17:52:43 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="p-4">
-      <h2>This is the Footer</h2>
+      <h2>Powered by <a href='https://www.youtube.com/@TheMythicDen/shorts'>The Mythic Den</a></h2>
     </div>
   );
 }
