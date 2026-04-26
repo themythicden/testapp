9decab5f1020b3065573f658d97c6a8e58abb38f
themythicden <themythicden@gmail.com>
2026-04-26 18:37:53 +0200

    Update ISOPage.jsx

--- src/pages/ISOPage.jsx
+++ src/pages/ISOPage.jsx
@@ -224,7 +224,7 @@ export default function ISOPage() {
   
 
 return (
-  {console.log("ISO STATE:", isoCards);}
+ 
   <div className="p-4 space-y-4">
     {/* ISO LIST */}
     <div className="bg-gray-800 p-3 rounded">
@@ -293,6 +293,7 @@ return (
         </React.Fragment>
       ))}
     </div>
+     {console.log("ISO STATE:", isoCards)}
   </div>
 );
 
