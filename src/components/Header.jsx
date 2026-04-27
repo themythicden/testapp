c699335f033ee56a108a2cf1b38e41944527df8f
themythicden <themythicden@gmail.com>
2026-04-27 02:07:21 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,7 +47,7 @@ export default function Header({ user }) {
                 My Collections
               </div>
 
-              <button onClick={() => navigate("/iso")}>
+              <button onClick={() => navigate("/")}>
                 ISO
               </button>
 
