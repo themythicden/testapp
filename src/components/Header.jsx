122256db7fdcd484f694579fd8a8a737151ed587
themythicden <themythicden@gmail.com>
2026-04-30 00:17:07 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import ProfileModal from "../components/ProfileModal";
 export default function Header({ user }) {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-  const [showProfile, setShowProfile] = useState(false);
+  const [showProfile, setShowProfile] = useState(true);
   const [profileOpen, setProfileOpen] = useState(false); 
   //const userEmail = user.email;
   //console.log("Usserrr: ", user.user_metadata.email);
