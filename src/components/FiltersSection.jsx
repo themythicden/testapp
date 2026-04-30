741e332af6fe49cf89be586aab9b81550eac5d02
themythicden <themythicden@gmail.com>
2026-04-30 04:49:40 +0200

    Update FiltersSection.jsx

--- src/components/FiltersSection.jsx
+++ src/components/FiltersSection.jsx
@@ -76,7 +76,7 @@ export default function FiltersSection({
       </div>
       {/* HEADER */}
       <div
-        onClick={() => (prev => !prev)}
+        onClick={() => setOpen(prev => !prev)}
         className="cursor-pointer p-3 flex justify-between items-center bg-gray-800"
       >
         <div className="flex items-center gap-2 text-white">
