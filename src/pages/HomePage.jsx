14bf615f970874a85c0dc99ebee05a94ea42d891
themythicden <themythicden@gmail.com>
2026-04-30 05:16:32 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,13 @@
-export default function HomePage() {
+export default function HomePage(user) {
   return (
     <div className="p-4">
+        {user ? (
+        <button onClick={() => navigate("/collections")}>
+        Collections
+        </button>
+      ) : (
+      
+      )};
     </div>
   );
 }
