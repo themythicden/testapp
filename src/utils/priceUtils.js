56a63840c1e0cdfb01bfe97f3518e925f10039f4
themythicden <themythicden@gmail.com>
2026-05-27 01:39:40 +0200

    Create priceUtils.js

---
+++ src/utils/priceUtils.js
@@ -0,0 +1,14 @@
+export const VARIANT_PRICE_MAP = {
+  normal: "normal",
+  holo: "holofoil",
+  reverse: "reverseHolofoil",
+  pokeball: "reverseHolofoil",
+  masterball: "reverseHolofoil"
+};
+
+export const CURRENCY_OPTIONS = [
+  { code: "USD", label: "USD - US Dollar", symbol: "$" },
+  { code: "ZAR", label: "ZAR - South African Rand", symbol: "R" },
+  { code: "EUR", label: "EUR - Euro", symbol: "€" },
+  { code: "GBP", label: "GBP - British Pound", symbol: "£" }
+];
