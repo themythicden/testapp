467d8f316bd6dd6d09b9f3d36c0488bd35628c3b
themythicden <themythicden@gmail.com>
2026-05-26 23:38:13 +0200

    Update SupertypeFilters.jsx

--- src/components/SupertypeFilters.jsx
+++ src/components/SupertypeFilters.jsx
@@ -23,7 +23,7 @@ export default function SupertypeFilters({
   };
 
   return (
-    <div className="flex justify-center items-center flex-wrap gap-3 w-full">
+    <div className="flex justify-center items-center flex-wrap gap-3 w-full m-1">
       {SUPERTYPES.map(({ name, icon }) => {
         const active = selected.includes(name);
 
