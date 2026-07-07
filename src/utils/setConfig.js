cff8a42517d8250eb24eb9f24dd19e38a26a6e14
themythicden <themythicden@gmail.com>
2026-07-07 21:52:48 +0200

    Update setConfig.js

--- src/utils/setConfig.js
+++ src/utils/setConfig.js
@@ -716,7 +716,7 @@ export const SET_CONFIG = {
         
   swsh10: {
       name: "Astral Radiance",
-      series: "Scarlet & Violet",
+      series: "Sword & SHield",
       releaseDate: "2022-05-27",
       standard: 189,
       extra: 27,
